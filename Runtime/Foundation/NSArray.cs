361a82b6c81ddc458b69c6cb870e5dc21f79ac0e
gil.reis <gil.reis@supercompany.io>
2022-10-17 09:00:38 -0300

    Make NSArray implement IReadOnlyCollection<Id>

--- Runtime/Foundation/NSArray.cs
+++ Runtime/Foundation/NSArray.cs
@@ -1,15 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace Gilzoide.ObjectiveC.Foundation
 {
-    public struct NSArray : IConvertibleToId
+    public struct NSArray : IConvertibleToId, IReadOnlyCollection<Id>
     {
         public static readonly Class Class = new Class("NSArray");
 
         [DllImport("__Internal", EntryPoint = "objc_msgSend")]
         public static extern NSArray objc_msgSend_idp_ulong(Id obj, Selector selector, Id[] objectArray, ulong length);
 
+        [DllImport("__Internal")]
+        public static extern long CFArrayGetCount(NSArray theArray);
+
+        [DllImport("__Internal")]
+        public static extern Id CFArrayGetValueAtIndex(NSArray theArray, long idx);
+
         private Id _self;
+
+        public int Count => (int) CFArrayGetCount(this);
+        public Id this[long index] => index >= 0 && index < Count
+            ? CFArrayGetValueAtIndex(this, (long) index)
+            : throw new IndexOutOfRangeException();
 
         public NSArray(Id self)
         {
@@ -36,6 +50,16 @@ namespace Gilzoide.ObjectiveC.Foundation
             return _self;
         }
 
+        public IEnumerator<Id> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
         public override string ToString()
         {
             return _self.ToString();
@@ -44,6 +68,37 @@ namespace Gilzoide.ObjectiveC.Foundation
         public static implicit operator Id(NSArray arr)
         {
             return arr._self;
+        }
+
+        public class Enumerator : IEnumerator<Id>
+        {
+            private NSArray _array;
+            private long _index;
+
+            public Enumerator(NSArray array)
+            {
+                _array = array;
+                _index = -1;
+            }
+
+            public Id Current => _array[_index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                _index++;
+                return _index < CFArrayGetCount(_array);
+            }
+
+            public void Reset()
+            {
+                _index = -1;
+            }
         }
     }
 }
